0bea8b533176e6b23b53fc6fd7981e7911396394
Thiago Costa <thiagohrcosta86@gmail.com>
2021-04-25 11:37:47 -0300

    Using child properties on react

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import Button from "./Button";
 function App() {
   return (
     <>
-    <Button title="Button 1"></Button>
-    <Button title="Button 2"></Button>
+    <Button>Button 1</Button>
+    <Button>Button 2</Button>
+    <Button>Button 3</Button>
     </>
   );
 }
