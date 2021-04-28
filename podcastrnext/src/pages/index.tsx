7e6e30dd0fc2127702049e7ff84c16ee29845035
Thiago Costa <thiagohrcosta86@gmail.com>
2021-04-28 19:31:28 -0300

    Adding episodes CSS

--- podcastrnext/src/pages/index.tsx
+++ podcastrnext/src/pages/index.tsx
@@ -4,7 +4,7 @@ import { format, parseISO} from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 import { converDurationToTimeString } from '../utils/convertDurationToTimeString';
 
-import styles from '.home.module.scss';
+import styles from './home.module.scss';
 
 type Episode = {
     id: string;
@@ -20,18 +20,26 @@ type Episode = {
 }
 
 type HomeProps = {
-  episodes: Episode[];
+  latestEpisodes: Episode[];
+  allEpisodes: Episode[];
 }
 
-export default function Home(props: HomeProps) {
+export default function Home( { latestEpisodes, allEpisodes }: HomeProps) {
   return (
     <div className={styles.homepage}>
       <section className={styles.latestEpisodes}>
         <h2>Últimos lançamentos</h2>
         <ul>
-
+            {latestEpisodes.map(episode => {
+              return (
+                <li key={episode.id}>
+                  <a href={episode.title}></a>
+                </li>
+              )
+            })}
         </ul>
       </section>
+      
       <section className={styles.allEpisodes}>
 
       </section>
