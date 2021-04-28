ba0667085b3b4a76c11a57c656e603bfe5251c9e
Thiago Costa <thiagohrcosta86@gmail.com>
2021-04-28 19:12:34 -0300

    Formating time

--- podcastrnext/src/pages/index.tsx
+++ podcastrnext/src/pages/index.tsx
@@ -1,5 +1,8 @@
 import { GetStaticProps} from 'next';
 import { api } from '../services/api';
+import { format, parseISO} from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import { converDurationToTImeString } from '../utils/convertDurationToTimeString';
 
 type Episode = {
     id: string;
@@ -16,7 +19,7 @@ export default function Home(props: HomeProps) {
   return (
     <div>
       <h1>Index</h1>
-      <p>{JSON.stringify(props.episodes)}</p>
+      <p>{}</p>
     </div>
   )
 }
@@ -28,6 +31,20 @@ export const getStaticProps: GetStaticProps = async () => {
       _sort: 'published_at',
       _order: 'desc'
     }
+  })
+
+  const episodes = data.map(episode => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', { locale: ptBR }),
+      duration: Number(episode.file.duration),
+      durationAsString: converDurationToTImeString(Number(episode.file.duration)),
+      description: episode.description,
+      ulr: episode.file.url,
+    };
   })
 
   return {
